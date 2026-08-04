f0b7364cd24474e075aa0ea5799d914ca1533fcf
verdonstudio <verdonstudio@gmail.com>
2026-08-04 21:24:52 +0200

    update redirect

--- redirect.js
+++ redirect.js
@@ -17,7 +17,13 @@ const reservations = {
         "evenements": "evenements.html?lang=FR&logement=salles8&edatestart=2026-08-09&edateend=2026-08-14",
         "map": "index.html?lang=FR&logement=salles8&zoom=12&pos=43.77258%2C6.20831&edatestart=2026-08-09&edateend=2026-08-14",
         "highlights": "highlights.html?edatestart=2026-08-09&edateend=2026-08-14&lang=FR&logement=salles8&ids=rec-bois-tourne-aiguines%2Cevt-12%2C1008PONEY%2Cevt-23%2Cevt-77%2C1108PETIT%2Crec-moustiers-nocturne-mercredi%2C1208MAMA%2C1208ACCRONOCT%2C1308SURF%2C1308GEO%2Cevt-36%2C1408CINE%2Cevt-78&from=evenements&visits=plage-salles%3A2026-08-09%2Cplage-margaridon%3A2026-08-10%2Cplage-galetas%3A2026-08-11%2Ctopo-lac-basses-gorges-mrkl4060%3A2026-08-12%2Crando-moustiers%3A2026-08-13%2Caiguines-table-orientation%3A2026-08-14",
-        "panorama": "panorama.html?lang=FR&logement=moustiers&edatestart=2026-08-06&edateend=2026-08-09"
+        "panorama": "panorama.html?lang=FR&logement=moustiers&edatestart=2026-08-09&edateend=2026-08-14"
+    },
+    "HMXCDR8882": {
+        "evenements": "evenements.html?lang=FR&logement=salles8&edatestart=2026-08-15&edateend=2026-08-21",
+        "map": "index.html?lang=FR&logement=salles8&zoom=12&pos=43.77258%2C6.20831&edatestart=2026-08-15&edateend=2026-08-21",
+        "highlights": "highlights.html?&edatestart=2026-08-15&edateend=2026-08-21&lang=FR&logement=salles8&ids=evt-79%2Cevt-80%2C1708PONEY%2Cevt-23%2C1808PATEVENT-SALLES%2Cevt-49%2Crec-boules-nocturnes-salles%2Crec-moustiers-nocturne-mercredi%2C1908ACCRONOCT%2Cevt-36%2C2008ASTRO%2C2008ASTROSOL%2C2108CINE%2Cevt-64b&from=evenements",
+        "panorama": "panorama.html?lang=FR&logement=moustiers&edatestart=2026-08-15&edateend=2026-08-21"
     },
     "default": {
         "evenements": "evenements.html",
