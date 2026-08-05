ed36ddb9f91eac2bae5247e6920c1a9f9cc66f0e
verdonstudio <verdonstudio@gmail.com>
2026-08-05 09:28:19 +0200

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -31,6 +31,37 @@ const reservations = {
         "highlights": "highlights.html",
         "panorama": "panorama.html",
         "parking" : "index.html?logement=moustiers&lang=FR&zoom=16.5&pos=43.84511%2C6.22124&cat=parking_gratuit%2Cparking_zonebleu%2Cparking_payant&fullscreen=1"
+    },
+
+    "HMHBKYZK28": {
+        "evenements": "evenements.html?lang=FR&logement=moustiers&edatestart=2026-08-17&edateend=2026-08-22",
+        "map": "index.html?lang=FR&logement=moustiers&zoom=13&pos=43.82945%2C6.21921&edatestart=2026-08-17&edateend=2026-08-22",
+        "highlights": "highlights.html?edatestart=2026-08-17&edateend=2026-08-22&lang=FR&logement=moustiers&ids=1708PONEY%2Cevt-23%2C1808PATEVENT-SALLES%2C1808PETIT%2Crec-stecroix-mardi%2Crec-moustiers-artisanat-mardi%2Cevt-27%2Cevt-48%2Cevt-49%2C1908ACCRONOCT%2Crec-aperitif-ferme-roumoules%2Crec-boules-nocturnes-salles%2Crec-riez-marche-mercredi%2Crec-moustiers-nocturne-mercredi%2Cevt-30%2Cevt-33%2Cevt-29%2C2008ASTROSOL%2C2008ASTRO%2Crec-riez-nocturne-jeudi%2Cevt-36%2Cevt-64b%2C2108CINE%2Cevt-16%2Cevt-81%2Cevt-82&from=evenements&visits=rando-moustiers%3A2026-08-18%2Cplage-galetas%3A2026-08-19%2Caiguines-table-orientation%3A2026-08-20%2Ctopo-lac-basses-gorges-mrkl4060%3A2026-08-21%2Cplage-salles%3A2026-08-22",
+        "panorama": "panorama.html?lang=FR&logement=moustiers&edatestart=2026-08-17&edateend=2026-08-22"
+    },
+    "HMKZPX4TMA": {
+        "evenements": "evenements.html?lang=FR&logement=salles8&edatestart=2026-08-22&edateend=2026-08-28",
+        "map": "index.html?lang=FR&logement=salles8&zoom=12&pos=43.77258%2C6.20831&edatestart=2026-08-22&edateend=2026-08-28",
+        "highlights": "highlights.html?edatestart=2026-08-22&edateend=2026-08-28&lang=FR&logement=salles8&ids=evt-81%2Cevt-82%2Crec-riez-marche-mercredi%2Cevt-30%2Cevt-29%2Cevt-48%2Cevt-12%2Cevt-13%2Cevt-23%2Crec-stecroix-mardi%2Crec-moustiers-artisanat-mardi%2Cevt-27%2Cevt-49%2Crec-aperitif-ferme-roumoules%2Crec-boules-nocturnes-salles%2Crec-moustiers-nocturne-mercredi%2Cevt-33%2C2708ASTROSOL%2C2708ASTRO%2Crec-riez-nocturne-jeudi%2Cevt-36%2Cevt-83%2Cevt-16&from=evenements&visits=plage-salles%3A2026-08-23%2Cplage-galetas%3A2026-08-24%2Caiguines-table-orientation%3A2026-08-25%2Crando-moustiers%3A2026-08-26%2Ctopo-lac-basses-gorges-mrkl4060%3A2026-08-27%2Cplage-margaridon%3A2026-08-28",
+        "panorama": "panorama.html?lang=FR&logement=salles8&edatestart=2026-08-22&edateend=2026-08-28"
+    },
+    "HMYRZQWQNF": {
+        "evenements": "evenements.html?lang=FR&logement=moustiers&edatestart=2026-08-31&edateend=2026-09-04",
+        "map": "index.html?lang=FR&logement=moustiers&zoom=13&pos=43.82945%2C6.21921&edatestart=2026-08-31&edateend=2026-09-04",
+        "highlights": "highlights.html?edatestart=2026-08-31&edateend=2026-09-04&lang=FR&logement=moustiers&ids=evt-23%2Crec-stecroix-mardi%2Cevt-48%2Cevt-49%2Crec-riez-marche-mercredi%2Cevt-30%2Cevt-33%2Cevt-29%2C0309ASTROSOL%2C0309ASTRO%2Cevt-36%2Cevt-16&from=evenements&visits=rando-moustiers%3A2026-09-01%2Cplage-galetas%3A2026-09-02%2Caiguines-table-orientation%3A2026-09-03%2Ctopo-lac-basses-gorges-mrkl4060%3A2026-09-04",
+        "panorama": "panorama.html?lang=FR&logement=moustiers&edatestart=2026-08-31&edateend=2026-09-04"
+    },
+    "HMMBAKWMFD": {
+        "evenements": "evenements.html?lang=FR&logement=salles6&edatestart=2026-09-08&edateend=2026-09-13",
+        "map": "index.html?lang=FR&logement=salles6&zoom=12&pos=43.77258%2C6.20831&edatestart=2026-09-08&edateend=2026-09-13",
+        "highlights": "highlights.html?edatestart=2026-09-08&edateend=2026-09-13&lang=FR&logement=salles6&ids=rec-stecroix-mardi%2Cevt-48%2Cevt-49%2Crec-riez-marche-mercredi%2Cevt-30%2Cevt-33%2Cevt-29%2C1009ASTROSOL%2C1009ASTRO%2Cevt-36%2Cevt-86%2C1109MAMA%2Cevt-16%2Cevt-12%2Cevt-13&from=evenements&visits=plage-salles%3A2026-09-09%2Cplage-galetas%3A2026-09-10%2Caiguines-table-orientation%3A2026-09-11%2Crando-moustiers%3A2026-09-12%2Ctopo-lac-basses-gorges-mrkl4060%3A2026-09-13",
+        "panorama": "panorama.html?lang=FR&logement=salles6&edatestart=2026-09-08&edateend=2026-09-13"
+    },
+    "HMDXYHZQZN": {
+        "evenements": "evenements.html?lang=FR&logement=salles6&edatestart=2026-09-25&edateend=2026-09-26",
+        "map": "index.html?lang=FR&logement=salles6&zoom=12&pos=43.77258%2C6.20831&edatestart=2026-09-25&edateend=2026-09-26",
+        "highlights": "highlights.html?edatestart=2026-09-25&edateend=2026-09-26&lang=FR&logement=salles6&ids=evt-16%2Cevt-87%2C2609MAMA%2Crec-riez-marche-mercredi%2Cevt-30%2Cevt-29%2Cevt-48&from=evenements&visits=plage-salles%3A2026-09-26",
+        "panorama": "panorama.html?lang=FR&logement=salles6&edatestart=2026-09-25&edateend=2026-09-26"
     }
 
 };
