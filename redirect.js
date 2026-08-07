f3af345d2be9855174bb6d1617976f0e0bd66615
verdonstudio <verdonstudio@gmail.com>
2026-08-07 18:38:36 +0200

    redirect default

--- redirect.js
+++ redirect.js
@@ -40,7 +40,7 @@ const reservations = {
         "map": "index.html",
         "highlights": "highlights.html",
         "panorama": "panorama.html",
-        "parking" : "index.html?logement=moustiers&lang=EN&zoom=16.5&pos=43.84511%2C6.22124&cat=parking_gratuit%2Cparking_zonebleu%2Cparking_payant&fullscreen=1",
+        "parking" : "index.html?logement=moustiers&lang=FR&zoom=16.5&pos=43.84511%2C6.22124&cat=parking_gratuit%2Cparking_zonebleu%2Cparking_payant&fullscreen=1",
         "parking-en" : "index.html?logement=moustiers&lang=EN&zoom=16.5&pos=43.84511%2C6.22124&cat=parking_gratuit%2Cparking_zonebleu%2Cparking_payant&fullscreen=1"
     },
     // Tag generique "que faire aujourd'hui" : pas lie a une reservation precise, pas de
