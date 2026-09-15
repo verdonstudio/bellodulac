417c85036d97bab8479df09368b64b42a7f104a3
verdonstudio <verdonstudio@gmail.com>
2026-09-15 08:19:50 +0200

    Update evenements.js

--- evenements.js
+++ evenements.js
@@ -3506,7 +3506,7 @@ window.EVENTS_DATA = {
       "freq": "weekly",
       "interval": 1,
       "startDate": null,
-      "endDate": null,
+      "endDate": "2026-08-31",
       "byDay": [
         "wed"
       ]
